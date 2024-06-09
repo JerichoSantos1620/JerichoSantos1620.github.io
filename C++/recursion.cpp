045395dba209dcf3983956b89cb3f5275e18f5c5
Jericho <jericho.santos1620@gmail.com>
2024-06-09 23:33:09 +0800

    sdfdgags

---
+++ C++/recursion.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main () {
+
+    // recursion = a programming technique where a function
+    // invokes itself from within
+    // break a complex concept into a repeatable single steps
+
+    // (iterative vs recursive)
+
+    // advantages = less coode and is cleaner 
+    // useful for sorting and searching algorithms
+
+    // disadvantages = uses more memory
+    // slower
+
+    return 0;
+}
