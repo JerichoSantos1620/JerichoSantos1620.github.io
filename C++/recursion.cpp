5a35beadee6f7c4ac67193e4726038975100b4bf
Jericho <jericho.santos1620@gmail.com>
2024-06-10 10:58:02 +0800

    dsfgafsadasf

--- C++/recursion.cpp
+++ C++/recursion.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
-
-int main () {
-
-    // recursion = a programming technique where a function
-    // invokes itself from within
-    // break a complex concept into a repeatable single steps
-
-    // (iterative vs recursive)
-
-    // advantages = less coode and is cleaner 
-    // useful for sorting and searching algorithms
-
-    // disadvantages = uses more memory
-    // slower
+int factorial(int num); 
+int main()
+{
+    std::cout << factorial(10);
 
     return 0;
+}
+int factorial(int num){
+    if(num > 1){
+        return num * factorial(num - 1);
+    }
+    else{
+        return 1;
+    }
 }
